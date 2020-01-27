11360fb477e9fceef9b3b96210d9aa9e5681bf1a
Shubham <shubh.chodankar@gmail.com>
2020-01-28 00:39:11 +0530

    adds profile upload

--- src/Signup.js
+++ src/Signup.js
@@ -18,6 +18,8 @@ import {
     MuiPickersUtilsProvider,
     KeyboardDatePicker,
 } from '@material-ui/pickers';
+import {DropzoneArea} from 'material-ui-dropzone'
+
 
 function Copyright() {
     return (
@@ -39,6 +41,13 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
         alignItems: 'center',
     },
+    paperMain: {
+        margin: theme.spacing(8),
+        marginTop : 0,
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+    },
     avatar: {
         margin: theme.spacing(1),
         backgroundColor: theme.palette.secondary.main,
@@ -58,6 +67,14 @@ const useStyles = makeStyles(theme => ({
     dob : {
         left: "3rem",
         top: "-2rem",
+    },
+    profileImage: {
+        border : "none",
+        // margin: "14rem 4rem",
+        background : "none"
+    },
+    profilePicContainer : {
+        marginTop: theme.spacing(24),
     }
 }));
 
@@ -66,139 +83,175 @@ export default function SignUp() {
     const classes = useStyles();
     // The first commit of Material-UI
     const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    const [uploadFile, setFiles] = React.useState([]);
+    const [previewURL, setpreviewURL] = React.useState(null);
 
     const handleDateChange = date => {
         setSelectedDate(date);
     };
+
+    const handleFileChange = files => {
+        // console.log("The file being uploaded is : ", files);
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            setFiles(files[0]);
+            setpreviewURL(e.target.result)
+        };
+        reader.readAsDataURL(files[0]);
+
+    }
     return (
-        <Container component="main" maxWidth="sm">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
+        <Grid container spacing={1} className={classes.paperMain}>
+            <Grid
+                item
+                md={4}
+                xs={12}
+                direction="row"
+                justify="center"
+                alignItems="center"
+                classes={{ root : classes.profilePicContainer }}
+            >
+                <DropzoneArea
+                    dropzoneText={previewURL? <img src={previewURL}/> :  "Profile Photo"}
+                    acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
+                    showPreviewsInDropzone={false}
+                    dropzoneClass={classes.profileImage}
+                    maxFileSize={5000000}
+                    onChange={handleFileChange}
+                />
+            </Grid>
+            <Grid item md={8} xs={12}>
+                <Container component="main" maxWidth="sm">
+                    <CssBaseline />
+                    <div className={classes.paper}>
+                        <form className={classes.form} noValidate>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} md={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                    />
+                                </Grid>
 
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <Grid container justify="space-around" className={classes.dateOfBirthWrapper}>
-                                <KeyboardDatePicker
-                                    margin="normal"
-                                    id="date-picker-dialog"
-                                    label="Your Birthday *"
-                                    format="MM/dd/yyyy"
-                                    value={selectedDate}
-                                    onChange={handleDateChange}
-                                    InputLabelProps={{
-                                        shrink : false,
-                                        classes : { root : classes.dob}
-                                    }}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change date',
-                                    }}
-                                />
+                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <Grid container justify="space-around" className={classes.dateOfBirthWrapper}>
+                                        <KeyboardDatePicker
+                                            margin="normal"
+                                            id="date-picker-dialog"
+                                            label="Your Birthday *"
+                                            format="MM/dd/yyyy"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            InputLabelProps={{
+                                                shrink : false,
+                                                classes : { root : classes.dob}
+                                            }}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </Grid>
+                                </MuiPickersUtilsProvider>
+                                <Grid item item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="telephone"
+                                        label="Contact Number"
+                                        id="telephone"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="address"
+                                        label="Address"
+                                        name="address"
+                                        autoComplete="lname"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        variant="outlined"
+                                        id="filled-required"
+                                        fullWidth
+                                        label="Which school did you attend?"
+                                        defaultValue=""
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        variant="outlined"
+                                        id="filled-required"
+                                        fullWidth
+                                        label="Where were you born?"
+                                        defaultValue=""
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        variant="outlined"
+                                        id="filled-required"
+                                        fullWidth
+                                        label="Your first pet's name?"
+                                        defaultValue=""
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                        label="I understand this is a jetcake coding test website"
+                                    />
+                                </Grid>
                             </Grid>
-                        </MuiPickersUtilsProvider>
-                        <Grid item item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
+                            <Button
+                                type="submit"
                                 fullWidth
-                                name="telephone"
-                                label="Contact Number"
-                                id="telephone"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="address"
-                                label="Address"
-                                name="address"
-                                autoComplete="lname"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                variant="outlined"
-                                id="filled-required"
-                                fullWidth
-                                label="Which school did you attend?"
-                                defaultValue=""
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                variant="outlined"
-                                id="filled-required"
-                                fullWidth
-                                label="Where were you born?"
-                                defaultValue=""
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                variant="outlined"
-                                id="filled-required"
-                                fullWidth
-                                label="Your first pet's name?"
-                                defaultValue=""
-                            />
-                        </Grid>
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </div>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </Container>
+            </Grid>
+        </Grid>
 
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I understand this is a jetcake coding test website"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={5}>
-                <Copyright />
-            </Box>
-        </Container>
     );
 }
